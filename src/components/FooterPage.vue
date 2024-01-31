3816f60a4ad990dffa192cf85a4c1ab262739b89
N0zik <mnvx.david@gmail.com>
2024-01-31 12:17:48 +0100

    Depot David 31/01/2024

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -2,46 +2,51 @@
   <footer>
     <div class="foot">
       <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <div>             
+              <img class="logo" href="www.twitter.com" src="@/assets/LogoWW.webp" alt="Logo Twiter" />            
+              <p class="copyright" >Gang Blue © 2024</p>
+            </div>           
             <div class="col1">
                 <ul class="list-inline">
                     <li class="list"><a href="#">Accueil</a></li>
-                    <li class="list"><a href="#">Services</a></li>
-                    <li class="list"><a href="#">About</a></li>
+                    <li class="list"><a href="#">A propos</a></li>
+                    <li class="list"><a href="#">Contact</a></li>
                 </ul>
             </div>                
             <div class="col2">
                 <ul>
-                    <li class="list"><a href="#">Terms</a></li>
-                    <li class="list"><a href="#">Privacy Policy</a></li>
-                    <li class="list"><a href="#">A propos</a></li>
+                    <li class="list"><a href="#">Catégories</a></li>
+                    <li class="list"><a href="#">Produits</a></li>
+                    <li class="list"><a href="#">Marques</a></li>
                 </ul>
             </div>                                 
             <div class="col3">
                 <ul class="list-inline">
                     <li class="list"><a href="#">Connexion</a></li>
                     <li class="list"><a href="#">S'inscrire</a></li>
-                    <li class="list"><a href="#"></a></li>
+                    <li class="list"><a href="#">Carrières</a></li>
+                    
                 </ul>
             </div> 
             <div class="col4">
                 <ul>
-                    <li class="list"><a href="#">Terms</a></li>
-                    <li class="list"><a href="#">Privacy Policy</a></li>
-                    <li class="list"><a href="#">Contact</a></li>                   
+                    <li class="list"><a href="#">Conditions générales de ventes</a></li>
+                    <li class="list"><a href="#">Conditions générales d'utilisations</a></li>
+                    <li class="list"><a href="#">Politique de confidencialité</a></li>                   
                 </ul>
             </div>
             <div class="col4">
                 <ul>
-                    
+                    <p>Suivez nous</p>
                     <img href="www.facebook.fr" src="@/assets/FB.svg" alt="Logo FaceBook"/>                                      
                     <img href="www.linkedin.fr" src="@/assets/LKD.svg" alt="Logo Linkedin" />               
                     <img href="www.twitter.com" src="@/assets/TWT.svg" alt="Logo Twiter" /> 
                                 
                 </ul>
-            </div>
+            </div>   
+                    
       </div>
-      
-          <p class="copyright">Gang Blue © 2024</p>
+          
   </footer>
 </template>
 
@@ -51,9 +56,12 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 footer {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
   background-color: #cca88c ;
   color: white;
   padding: 10px;
@@ -61,13 +69,25 @@ footer {
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: 170px;
-  
+  height: 150px;
+  padding-bottom: 20px;
   
 }
+  
+.foot{
+  margin-top: auto;
+  gap: 50px;
+  display: flex;
+  justify-content: center;
+  gap: 70px;
+  margin-left: -104px;
+}
+
 
 .list{
-  display: grid;
+  display: flex;
+  justify-content:center ;
+  margin-bottom: 20px;
   flex-direction:column;
   margin: 0 10px;
   color: white;
@@ -75,17 +95,13 @@ footer {
   padding: 2px;
    
 }
-a{
+a, p{
+  font-weight: 450;
   color: #5E3C1A;
   text-decoration: none;
 }
 
-.foot{
-  display: flex;
-  justify-content:center ;
-  margin-bottom: 20px;
 
-}
 
 .copyright{
   color: #5E3C1A;
@@ -97,6 +113,12 @@ a{
 img{
   width: 30px;
   height: 30px;
+  margin: 0 10px;
+}
+
+.logo{
+  width: 100px;
+  height: 100px;
   margin: 0 10px;
 }
 
