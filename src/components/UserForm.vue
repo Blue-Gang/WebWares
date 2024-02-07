14252484bf2b172b562947578bd7fae3dd0b824a
CecileYhy <gastrin.ce@gmail.com>
2024-02-07 10:59:46 +0100

    modifCec7fev

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,24 +1,45 @@
 <template>
   <div class="container">
-    <form v-on:submit.prevent class="formulaire">
+
+    <form 
+    @submit.prevent="checkForm" 
+    class="formulaire">
 
     <h2 class="title">{{ title }}</h2>
     <p>{{ paragraphe }}</p>
 
 
-
     <div v-if="formInscription" class="company">
         <br>
-        <label v-if="formInscription" class="label">Informations de l'entreprise :</label>
+        <label 
+        v-if="formInscription" 
+        class="label" for="companyName">Informations de l'entreprise :</label>
         <br>
-      <input type="text" placeholder="Raison sociale (nom de l'entreprise)*">
-      <input v-model="siret" type="text" placeholder="Numéro de SIRET (14 chiffres)*">
-    
+      <input 
+      v-model="raisonSociale" 
+      type="text" 
+      placeholder="Raison sociale (nom de l'entreprise)*" >
+
+      <input 
+      v-model="siret" 
+      type="text" 
+      placeholder="Numéro de SIRET (14 chiffres)*" >
         <div class="adress">
-            <input type="text" placeholder="Adresse complète de l'entreprise*" class="inputadress">
+            <input 
+            v-model="adresse" 
+            type="text" 
+            placeholder="Adresse complète de l'entreprise*" 
+            class="inputadress">
             <div class="cp">
-            <input v-model="cp" type="text" placeholder="Code Postal">
-            <input type="text" placeholder="Ville" class="city">
+            <input 
+            v-model="codePostal" 
+            type="number" 
+            placeholder="Code Postal*">
+            <input 
+            v-model="ville" 
+            type="text" 
+            placeholder="Ville*" 
+            class="city">
             </div>
         </div>
     </div>
@@ -27,17 +48,49 @@
     <label v-if="formInscription" class="label">Vos informations personnelles :</label>
       <br>
 
-        <input type="email" placeholder="Email*">
-        <input type="password" placeholder="Mot de passe*">
-        <input type="password" placeholder="Confirmer le mot de passe*">
+        <input 
+        v-model="email" 
+        type="email" 
+        placeholder="Email*">
+        <input 
+        v-model="password.password" 
+        type="password" 
+        placeholder="Mot de passe*">
+        <input 
+        v-model="password.confirmPassword" type="password" 
+        placeholder="Confirmer le mot de passe*">
 
-    <button @click="validate()" type="submit">Me connecter</button>
 
+      <button v-if="!formInscription"
+      v-on:click.stop
+       @click="checkForm()"
+      type="submit">Se connecter</button>
+
+
+
+    <router-link 
+     to='/'>
+      <button v-if="formInscription" v-on:click.stop @click="checkForm()" 
+      type="submit">S'inscrire</button>
+    </router-link>
+
+
+
+    <div v-if="!formInscription">
+      <p class="nouv">Pas encore de compte chez nous ?</p>
+      <button class="btnCreer">
+        <router-link to="/signup" class="btnInscription">S'inscrire</router-link>
+        </button>
+    </div>
+    
     </form>
-  </div>
+    </div>
+
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
 
   data() {
@@ -68,18 +121,107 @@ export default {
             default: 'Ravis de vous revoir !'
         },
 
+
         paragraphe: {
             type: String,
             default: 'Identifiez-vous pour profiter de tous nos produits.'
         },
 
-        formInscription: Object,
+        formInscription: {
+          type: Boolean,
+          default: false,
+        },
+
+        // formInscription: Object,
 
 
     },
+  computed:{
+    ...mapState(['clients']),
+  },
+
+  data() {
+    return {
+      error: [],
+      raisonSociale: '',
+      siret: '',
+      adresse: '',
+      cp: '',
+      codePostal: '',
+      ville: '',
+      email: '',
+      password: {
+        password: '',
+        confirmPassword: '',
+      },
+      
+
+     
+      
+
+    }
+    },
+
+  methods: {
+    checkForm() {
+
+      this.$store.dispatch('clientEnLigne', true);
+
+      this.$emit('connexionUtilisateur', true);
+
+      if(!this.formInscription){
+      if(!this.email || !this.password.password || !this.password.confirmPassword){
+        alert('Veuillez remplir tous les champs obligatoires.')
+        return
+      }}
+
+      if(this.password.password != this.password.confirmPassword) {
+        alert('Les mots de passes ne correspondent pas.')
+        return
+      }
 
 
+      if(this.formInscription) {
+        if(!this.raisonSociale || !this.siret || !this.adresse || !this.codePostal || !this.ville){
+          alert('Veuillez remplir toutes les informations de l\'entreprise.');
+          return
+        }
+        if(this.password.password != this.password.confirmPassword) {
+        alert('Les mots de passes ne correspondent pas.')
+        return
+        }
+        if(!this.siret || this.siret.length !== 14 || !/^\d+$/.test(this.siret)) {
+          alert('Le numéro SIRET doit comporter 14 chiffres.');
+          return
+        }
+        if(!this.email || !this.password.password || !this.password.confirmPassword) {
+          alert('Veuillez renseigner vos informations personnelles.');
+          return
+        }
+
+        const userData = {
+          raisonSociale: this.raisonSociale,
+          siret: this.siret,
+          adresse: this.adresse,
+          codePostal: this.codePostal,
+          ville: this.ville,
+          email: this.email,
+    
+
+        };
+
+        localStorage.setItem('userData', JSON.stringify(userData))
+      }
+
+      if(this.email || this.password.password || this.password.confirmPassword) {
+        this.$router.push('/');
+      }
+
+  },
+
+  }
 }
+
 </script>
 
 <style scoped>
@@ -88,7 +230,7 @@ export default {
   /* background-color: red; */
   width: 400px;
   height: 800px;
-  margin: 170px auto;
+  margin: 0 auto;
 }
 
 .formulaire{
@@ -96,7 +238,7 @@ export default {
   background-color: #dec5b1;
   /* width: 600px; */
   height: auto;
-  margin: 0 auto;
+  margin: 100px auto;
   padding: 20px;
   border-radius: 10px;
   color: #5e3c1a;
@@ -105,6 +247,9 @@ export default {
 
 }
 
+.title{
+  margin-bottom: 20px;
+}
 
 .formulaire input{
   margin: 5px;
@@ -134,6 +279,22 @@ button:hover {
   color: #cca88c;
 }
 
+
+.nouv{
+  padding-top: 40px;
+  font-size: 15px;
+  margin-bottom: -15px;
+}
+
+.btnCreer{
+  width: 80px;
+  height: 30px;
+  font-size: 12px;
+  
+}
+
+
+
 label{
   float: left;
   margin-bottom: 10px;
@@ -145,6 +306,13 @@ label{
   width: 90px;
 }
 
+.btnInscription{
+  text-decoration: none;
+  color: #5e3c1a;
+}
 
+.btnInscription:hover {
+  color: #cca88c;
+}
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,14 @@ import HomeView from '../views/HomeView.vue'
 import PageContact from '../views/PageContact.vue'
 import LoginPage from '@/views/LoginPage.vue'
 import SignUp from '@/views/SignUp.vue'
+import PageCgv from '@/views/PageCgv.vue'
+import AdminVue from '@/views/AdminVue'
+import PagePartenaire from '@/views/PagePartenaire.vue'
+import PageCgu from '@/views/PageCgu.vue'
+import PolitiqueConf from '@/views/PolitiqueConf.vue'
+import ModifUtilisateur from '@/views/ModifUtilisateur.vue'
+import ListeClients from '@/views/ListeClients.vue'
+
 
 const routes = [
   {
@@ -24,7 +32,43 @@ const routes = [
     path: '/signup',
     name: 'inscription',
     component: SignUp
-  }
+  },
+  {
+    path: '/adminvue',
+    name: 'admin',
+    component: AdminVue
+  },
+  {
+    path: '/pagecgv',
+    name: 'cgv',
+    component: PageCgv
+  },
+  {
+  path: '/pagepartenaire',
+  name: 'partenaire',
+  component: PagePartenaire
+},
+{
+  path: '/pagecgu',
+  name: 'cgu',
+  component: PageCgu
+},
+{
+  path: '/politiqueconf',
+  name: 'politiqueconf',
+  component: PolitiqueConf
+},
+{
+  path: '/modifutilisateur',
+  name: 'modifutilisateur',
+  component: ModifUtilisateur
+},
+{
+  path: '/listeclients',
+  name: 'listeclients',
+  component: ListeClients
+},
+ 
 ]
 
 const router = createRouter({
